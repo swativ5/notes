363cecaeeaef97397f6f47210f4212e171d74167
swativ5 <swativ.05.v@gmail.com>
2026-01-22 11:40:37 +0530

    notez

---
+++ computer vision/bayer.py
@@ -0,0 +1,10 @@
+GRGB_Mat = [[120, 200, 130],
+       [100, 150, 110],
+       [125, 105, 220]]
+
+def get_rgb_mat(mat, pattern='GRGB'):
+    row_pattern = pattern[0::2]
+    col_pattern = pattern[1::2]
+    
+    return GRGB_Mat
+
